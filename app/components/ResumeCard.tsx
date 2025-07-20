902c09abb595cddbec90b20fef431e52e2ea6780
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-07-20 20:32:46 +0530

    Update ResumeCard.tsx

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -34,7 +34,7 @@ const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath }
                 <div className="gradient-border animate-in fade-in duration-1000">
                     <div className="w-full h-full">
                         <img
-                            src={resumeUrl}
+                            src={imagePath}
                             alt="resume"
                             className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
                         />
